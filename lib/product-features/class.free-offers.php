5f6375853663cfe07d2bb1ad957a35d409d45a41
Glenn Ansley <glenn@ithemes.com>
2013-11-12 16:15:31 -0600

    Change text field labels in Advanced Settings.\n Add form field labels to checkboxes

--- lib/product-features/class.free-offers.php
+++ lib/product-features/class.free-offers.php
@@ -123,19 +123,19 @@ class IT_Exchange_Addon_Free_Offers_Product_Feature {
 	
 		?>
 		<div class="hide-price-label-settings">
-			<input type="checkbox" name="it-exchange-product-feature-free-offers[hide-price-in-store]" value="true" <?php checked( true, $values['hide-price-in-store'] ); ?>/>&nbsp;
-			<?php _e( 'Hide product price in Exchange store?', 'LION' ); ?><br />
+			<label><input type="checkbox" name="it-exchange-product-feature-free-offers[hide-price-in-store]" value="true" <?php checked( true, $values['hide-price-in-store'] ); ?>/>&nbsp;
+			<?php _e( 'Hide product price in Exchange store?', 'LION' ); ?></label><br />
 
-			<input type="checkbox" name="it-exchange-product-feature-free-offers[hide-price-on-product-page]" value="true" <?php checked( true, $values['hide-price-on-product-page'] ); ?>/>&nbsp;
-			<?php _e( 'Hide product price on Exchange product page?', 'LION' ); ?><br />
+			<label><input type="checkbox" name="it-exchange-product-feature-free-offers[hide-price-on-product-page]" value="true" <?php checked( true, $values['hide-price-on-product-page'] ); ?>/>&nbsp;
+			<?php _e( 'Hide product price on Exchange product page?', 'LION' ); ?></label><br />
 		</div>
 
 		<div class="button-labels">
 			<div class="buy-now-label">
-				<?php _e( 'Buy Now button', 'LION' ); ?>&nbsp;<input type="text" value="<?php esc_attr_e( $values['buy-now-label'] ); ?>" name="it-exchange-product-feature-free-offers[buy-now-label]" />
+				<?php _e( 'Buy Now Button Label', 'LION' ); ?>&nbsp;<input type="text" value="<?php esc_attr_e( $values['buy-now-label'] ); ?>" name="it-exchange-product-feature-free-offers[buy-now-label]" />
 			</div>
 			<div class="complete-purchase-label">
-				<?php _e( 'Complete Purchase button', 'LION' ); ?>&nbsp;<input type="text" value="<?php esc_attr_e( $values['complete-purchase-label'] ); ?>" name="it-exchange-product-feature-free-offers[complete-purchase-label]" />
+				<?php _e( 'Purchase Button Label', 'LION' ); ?>&nbsp;<input type="text" value="<?php esc_attr_e( $values['complete-purchase-label'] ); ?>" name="it-exchange-product-feature-free-offers[complete-purchase-label]" />
 			</div>
 		</div>
 		<?php

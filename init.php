5b242804da3ea66798d5aa4e3a4620bd68dadfd2
Glenn Ansley <glenn@ithemes.com>
2013-11-19 14:55:54 -0600

    Add version requirement nag

--- init.php
+++ init.php
@@ -101,3 +101,28 @@ function it_exchange_free_offers_addon_admin_wp_enqueue_styles( $hook_suffix, $p
 	}
 }
 add_action( 'it_exchange_admin_wp_enqueue_styles', 'it_exchange_free_offers_addon_admin_wp_enqueue_styles', 10, 2 );
+
+/**
+ * Shows the nag when needed.
+ *
+ * @since 1.0.2
+ *
+ * @return void
+*/
+function it_exchange_addon_free_offers_show_version_nag() {
+    if ( $GLOBALS['it_exchange']['version'] < '1.6.2' ) { 
+        ?>  
+        <div id="it-exchange-add-on-min-version-nag" class="it-exchange-nag">
+            <?php printf( __( 'The Free Offers add-on requires iThemes Exchange version 1.6.2 or greater. %sPlease upgrade Exchange%s.', 'LION' ), '<a href="' . admin_url( 'update-core.php' ) . '">', '</a>' ); ?>
+        </div>
+        <script type="text/javascript">
+            jQuery( document ).ready( function() {
+                if ( jQuery( '.wrap > h2' ).length == '1' ) {
+                    jQuery("#it-exchange-add-on-min-version-nag").insertAfter('.wrap > h2').addClass( 'after-h2' );
+                }
+            });
+        </script>
+        <?php
+    }   
+}
+add_action( 'admin_notices', 'it_exchange_addon_free_offers_show_version_nag' );

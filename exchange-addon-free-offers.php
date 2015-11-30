876f317571178daf867f8e1358b068b1e59ba523
Lew Ayotte <lew@ithemes.com>
2015-11-30 13:36:14 -0600

    Tagging 1.2.0

--- exchange-addon-free-offers.php
+++ exchange-addon-free-offers.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Free Offers
- * Version: 1.1.0
+ * Version: 1.2.0
  * Description: Allows you to turn any product into a free offer
  * Plugin URI: http://ithemes.com/purchase/free-offers-add-on/
  * Author: iThemes

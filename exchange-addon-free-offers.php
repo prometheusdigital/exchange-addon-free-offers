2c2df1d22aa97813af08951a814c2e0fe623b02d
AJ Morris <ajmorris2002@gmail.com>
2017-08-11 11:37:32 -0400

    Grab correct update system

--- exchange-addon-free-offers.php
+++ exchange-addon-free-offers.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: ExchangeWP - Free Offers
- * Version: 1.2.0
+ * Version: 0.0.1
  * Description: Allows you to turn any product into a free offer
  * Plugin URI: http://ithemes.com/purchase/free-offers-add-on/
  * Author: ExchangeWP
@@ -78,12 +78,12 @@ if ( ! class_exists( 'EDD_SL_Plugin_Updater' ) )  {
  	// retrieve our license key from the DB
  	// this is going to have to be pulled from a seralized array to get the actual key.
  	// $license_key = trim( get_option( 'exchange_freeoffers_license_key' ) );
- 	$exchangewp_freeoffers_options = get_option( 'it-storage-exchange_addon_freeoffers' );
- 	$license_key = $exchangewp_freeoffers_options['freeoffers_license'];
-
+ 	$exchangewp_freeoffers_options = get_option( 'it-storage-exchange_free_offers-addon' );
+ 	$license_key = $exchangewp_freeoffers_options['free_offers-license-key'];
+	
  	// setup the updater
  	$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
- 			'version' 		=> '1.2.2', 				// current version number
+ 			'version' 		=> '0.1.1', 				// current version number
  			'license' 		=> $license_key, 		// license key (used get_option above to retrieve from DB)
  			'item_name' 	=> 'free-offers', 	  // name of this plugin
  			'author' 	  	=> 'ExchangeWP',    // author of this plugin

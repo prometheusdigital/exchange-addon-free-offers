7c4b9df45260d542a289aea7820c76a672a648a4
AJ Morris <aj@ajmorris.me>
2018-01-08 22:25:00 -0500

    id from name and version bump

--- exchange-addon-free-offers.php
+++ exchange-addon-free-offers.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: ExchangeWP - Free Offers
- * Version: 0.0.1
+ * Version: 1.2.2
  * Description: Allows you to turn any product into a free offer
  * Plugin URI: http://ithemes.com/purchase/free-offers-add-on/
  * Author: ExchangeWP
@@ -78,9 +78,9 @@ function exchangewp_freeoffers_plugin_updater() {
 		$license = $license_key['exchange_license'];
 
 		$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
-				'version' 		=> '0.0.1', 				// current version number
-				'license' 		=> $license, 		// license key (used get_option above to retrieve from DB)
-				'item_name' 	=> urlencode('Free Offers'), 	  // name of this plugin
+				'version' 		=> '1.2.2', 				// current version number
+				'license' 		=> $license, 				// license key (used get_option above to retrieve from DB)
+				'item_id' 		=> 364,				 	  	// name of this plugin
 				'author' 	  	=> 'ExchangeWP',    // author of this plugin
 				'url'       	=> home_url(),
 				'wp_override' => true,
